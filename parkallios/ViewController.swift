a6834d2cb8695e861d79cf84a8a82da97e16f2c0
bozanilda <nilda.boza@tecsup.edu.pe>
2024-11-17 07:42:46 -0500

    Subida inicial del proyecto

---
+++ parkallios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  parkallios
+//
+//  Created by Nilda Boza on 12/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ parkalliosTests/parkalliosTests.swift
@@ -0,0 +1,17 @@
+//
+//  parkalliosTests.swift
+//  parkalliosTests
+//
+//  Created by Nilda Boza on 12/11/24.
+//
+
+import Testing
+@testable import parkallios
+
+struct parkalliosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
